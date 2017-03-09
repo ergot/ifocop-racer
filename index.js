6ebafb11f1d8a45885045473e52714c7bc92ccd7
ergot <wjose14@hotmail.com>
2017-03-09 15:07:51 +0100

    mongoose on

--- index.js
+++ index.js
@@ -2,8 +2,36 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
 
+// config
 app.use(express.static('./public'));
+mongoose.connect('mongodb://localhost/racer');
+
+//database player schema
+var playerSchema = mongoose.Schema({
+  speudo: String
+});
+//database player model
+var Player = mongoose.model('Player', playerSchema);
+
+var player1 = new Player({speudo: "player1"});
+
+player1.save(function (err, player1) {
+  if (err) return console.error(err);
+  console.log('save player1');
+});
+
+
+
+// database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('connection a la base');
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
